3c35d1ca050a9173f680857443f2007164b605eb
Patrick <gather4real@yahoo.com>
2021-03-20 20:57:59 +0000

    Added placeholder to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,7 @@ from .models import Contact
 
 class ContactForm(forms.ModelForm):
     """ Full Contact form ommiting responded field """
+    required_css_class = 'required'
 
     class Meta:
         model = Contact
@@ -14,3 +15,18 @@ class ContactForm(forms.ModelForm):
             'Email_Address',
             'Main_Message',
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Full_Name'].widget.attrs.update({
+            'class': 'text-muted',
+            'placeholder': 'Full Name'})
+        self.fields['Phone_Number'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'tel'})
+        self.fields['Email_Address'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'E-mail'})
+        self.fields['Main_Message'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Your Message...'})
