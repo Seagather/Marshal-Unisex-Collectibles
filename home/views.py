7175c64e7e90f090c721318406820b02c2dae5b2
Patrick <gather4real@yahoo.com>
2021-04-30 09:40:09 +0000

    testing 404.html page continues

--- home/views.py
+++ home/views.py
@@ -5,3 +5,11 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def http404(request):
+    return render(request, "404.html")
+
+
+def http500(request):
+    return render(request, "500.html")
